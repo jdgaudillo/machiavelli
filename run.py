54c13c79bad4954e35a443f34c8f6270d0ad3012
jdgaudillo <joverlyn.gaudillo@digenomix.com>
2019-08-08 15:51:47 +0800

    Add logging

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_restful import Api
+import logging
 
 from src.predict import Prediction
 
 app = Flask(__name__)
 api = Api(app)
 app.config["DEBUG"] = True
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 api.add_resource(Prediction, "/predictions")
 api.add_resource(Diagnosis, "/")
